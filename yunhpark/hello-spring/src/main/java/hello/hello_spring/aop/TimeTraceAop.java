feb1b9a299fb34128cd06662d767955360c3ef04
yunho772456 <165309414+yunho772456@users.noreply.github.com>
2025-02-28 23:32:28 +0900

    week 3

---
+++ yunhpark/hello-spring/src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package hello.hello_spring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hello_spring..*(..))") // 원하는 적용대상 선택하는 구문
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed(); // 꿀팁: ctrl alt n 인라인 합치기
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
